29e85a4f9ad6ee10f33dd8e61f46f4e433297a57
aabbcc <iiooiio@hotmail.com>
2019-04-01 21:13:34 +0800

    測試server不斷開連接 並每3秒回傳時間戳記給client

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import socket
+import time
+
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -8,6 +12,15 @@ s.listen(5)
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
+
+    msg = "Welcome to the server! 我是中文"
+    msg = (f'{len(msg):<{HEADERSIZE}}'+msg)
     
+    clientsocket.send(bytes(msg, "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is: {time.time()}"
+        msg = (f'{len(msg):<{HEADERSIZE}}'+msg)
+        clientsocket.send(bytes(msg, "utf-8"))
+
